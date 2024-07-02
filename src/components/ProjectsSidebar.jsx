6d69d2db713e03c846797ecef76606291a123213
Jan Hunar <jan.hunar.jha@gmail.com>
2024-07-02 13:34:20 +0200

    first commit

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const ProjectsSidebar = () => {
+  return (
+    <aside>
+        <h2>Your projects</h2>
+        <div>
+            <button>
+                + Add project
+            </button>
+        </div>
+        <ul></ul>
+    </aside>
+  )
+}

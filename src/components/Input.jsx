05216143d2401fecddcf9472a384282988b984d5
Jan Hunar <jan.hunar.jha@gmail.com>
2024-07-03 14:40:00 +0200

    09-sidebar: Before validation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { forwardRef } from "react";
 
-export const Input = ({ label, textarea, ...props }) => {
-
-    const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
 
   return (
     <p className="flex flex-col gap-1 my-4">
@@ -10,13 +11,12 @@ export const Input = ({ label, textarea, ...props }) => {
         {label}
       </label>
       {textarea ? (
-        <textarea
-          className={classes}
-          {...props}
-        />
+        <textarea ref={ref} className={classes} {...props} />
       ) : (
-        <input className={classes} {...props} />
+        <input ref={ref} className={classes} {...props} />
       )}
     </p>
   );
-};
+});
+
+export default Input;
